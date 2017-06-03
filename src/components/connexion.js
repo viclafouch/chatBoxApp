f2209e3e04878f2f46720ddceb2b0480eb02d017
Victor de la Fouchardiere <victor.dlf@outlook.fr>
2017-06-03 19:24:18 +0200

    add rooting to chat

--- src/components/connexion.js
+++ src/components/connexion.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 class Connexion extends React.Component {
+	goToChat = event => {
+		event.preventDefault();
+		console.log(this.pseudoInput.value);
+	};
+
 	render() {
 		return (
-			<div className="connexionBox">
+			<div className="connexionBox" onSubmit={e => this.goToChat(e)}>
 				<form className="connexion">
-					<input type="text" placeholder="pseudo" required />
+					<input type="text" placeholder="pseudo" required ref={input => {this.pseudoInput = input}} />
 					<button type="submit">Go</button>
 				</form>
 			</div>
--- src/index.js
+++ src/index.js
@@ -3,9 +3,23 @@ import React from 'react';
 import { render } from 'react-dom';
 import Connexion from './components/connexion';
 import App from './components/app';
+import NotFound from './components/notfound';
 import './index.css';
+import { BrowserRouter, Match, Miss } from 'react-router';
+
+const Root = () => {
+	return (
+		<BrowserRouter>
+			<div>
+				<Match exactly pattern="/" component={Connexion}/>
+				<Match exactly pattern="/pseudo/:pseudo" component={App}/>
+				<Miss component={NotFound} />
+			</div>
+		</BrowserRouter>
+	)
+}
 
 render(
-	<App />,
+	<Root />,
 	document.getElementById('root')
 );
