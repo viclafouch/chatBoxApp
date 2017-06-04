f1ecff59a8c51edb07b055a70230ff9532dbecf5
Victor de la Fouchardiere <victor.dlf@outlook.fr>
2017-06-04 16:30:57 +0200

    connnected to firebase

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyAC5hgsFQeME-bvje2I8ym69espexylWgg",
+    authDomain: "ma-chatbox-351c9.firebaseapp.com",
+    databaseURL: "https://ma-chatbox-351c9.firebaseio.com",
+});
+
+export default base;	
--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Formulaire from './formulaire';
 import Message from './message';
+import base from '../base';
 
 class App extends React.Component {
 
@@ -8,10 +9,18 @@ class App extends React.Component {
 		messages: {}
 	}
 
+	componentWillMount() {
+		this.ref = base.syncState('/', {
+			context: this,
+			state: 'messages'
+		})
+	}
+
 	addMessage = message => {
 		const messages = {...this.state.messages};
 		const timestamp = Date.now();
 		messages[`message-${timestamp}`] = message;
+		Object.keys(messages).slice(0, -10).map(key => messages[key] = null);
 		this.setState({ messages });
 	};
 
