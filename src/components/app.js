c8e03a7dbd58df051c70e2d554fe728266fb5d07
Victor de la Fouchardiere <victor.dlf@outlook.fr>
2017-06-04 20:45:25 +0200

    add custom css & component update

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Formulaire from './formulaire';
 import Message from './message';
 import base from '../base';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import '../animation.css';
 
 class App extends React.Component {
 
@@ -16,6 +18,10 @@ class App extends React.Component {
 		})
 	}
 
+	componentDidUpdate() {
+		this.messages.scrollTop = this.messages.scrollHeight;
+	}
+
 	addMessage = message => {
 		const messages = {...this.state.messages};
 		const timestamp = Date.now();
@@ -24,15 +30,27 @@ class App extends React.Component {
 		this.setState({ messages });
 	};
 
+	isUser = (pseudo) => {
+		return pseudo === this.props.params.pseudo;
+	};
+
 	render() {
 		const messages = Object
 		.keys(this.state.messages)
-		.map(key => <Message key={key} details={this.state.messages[key]}/>)
+		.map(key => <Message key={key} details={this.state.messages[key]} isUser={this.isUser}/>)
 		return (
 			<div className="box">
 				<div>
-					<div className="messages">
-						{messages}
+					<div className="messages" ref={input => this.messages = input} >
+						<ReactCSSTransitionGroup
+							component="div"
+							className="message"
+							transitionName="message"
+							transitionEnterTimeout={200}
+							transitionLeaveTimeout={200}
+						>
+							{messages}
+						</ReactCSSTransitionGroup>
 					</div>
 					<Formulaire addMessage={this.addMessage} pseudo={this.props.params.pseudo} length="140" />
 				</div>
--- src/components/message.js
+++ src/components/message.js
@@ -1,11 +1,28 @@
 import React from 'react';
 
 class Message extends React.Component {
+
+	preRender = (isUser) => {
+		if (isUser) {
+			return (
+				<p className="user-message">
+					{this.props.details.message}
+				</p>
+			)
+		}
+		else {
+			return (
+				<p className="not-user-message">
+					<b>{this.props.details.pseudo}</b>: 
+					{this.props.details.message}
+				</p>
+			)
+		}
+	};
+
 	render() {
 		return (
-			<p className="user-message">
-				{this.props.details.pseudo}: {this.props.details.message} 
-			</p>
+			this.preRender(this.props.isUser(this.props.details.pseudo))
 		)
 	}
 }
